6e85957a786a322c48f42ec1c955ab0214f14fce
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2023-09-23 19:21:02 +0530

    create server and connect to the database

---
+++ server.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { createPool } = require("mysql2");
+
+const pool = createPool({
+  host: "localhost",
+  port: 3310, // Correct the port if needed
+  user: "root",
+  password: "Udee1234@#",
+  database: "location",
+  connectionLimit: 10,
+});
+
+pool.query("SELECT * FROM stores", (err, result, fields) => {
+  if (err) {
+    console.error("Error:", err);
+    return;
+  }
+  console.log("Result:", result);
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(8880, () => {
+  console.log("Connected to backend!");
+});
