30954fcb7698e2c2d92e7ec34da074d9e2653384
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2023-09-24 15:43:40 +0530

    comlpete store given addres by frontend to the database

--- Backend/public/js/add.js
+++ Backend/public/js/add.js
@@ -1,1 +1,37 @@
+const storeForm = document.getElementById("store-form");
+const storeAddress = document.getElementById("store-address");
 
+// Send POST to API to add store
+async function addStore(e) {
+  e.preventDefault();
+
+  if (storeAddress.value === "") {
+    alert("Please fill in fields");
+  }
+
+  const sendBody = {
+    address: storeAddress.value
+  };
+
+  try {
+    const res = await fetch("/api/v1/stores", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(sendBody),
+    });
+
+    if (res.status === 400) {
+      throw Error("Store already exists!");
+    }
+
+    alert("Store added!");
+    window.location.href = "/index.html";
+  } catch (err) {
+    alert(err);
+    return;
+  }
+}
+
+storeForm.addEventListener("submit", addStore);
